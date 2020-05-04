5c48f64c3f1aa7d5b1b18fb88737dda6546f2e12
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-05-04 23:52:54 +0500

    проверка формата данных - временно

--- src/personalfinance/PersonalFinance.java
+++ src/personalfinance/PersonalFinance.java
@@ -1,10 +1,12 @@
 package personalfinance;
 
+import personalfinance.settings.Format;
 import personalfinance.settings.Settings;
 import personalfinance.settings.Text;
 
 import java.awt.*;
 import java.io.IOException;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,6 +14,7 @@ public class PersonalFinance {
 
     public static void main(String[] args) {
         init();
+        System.out.println(Format.dateMonth(new Date())); // проверка формата даты
 //        System.out.println(Text.get("PROGRAMM_NAME"));
 //        System.out.println(Arrays.toString(Text.getMonths ()));
     }
