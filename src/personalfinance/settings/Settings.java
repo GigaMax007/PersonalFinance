c432352d50bbfe402e2d7f07e3982c33c063b283
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-05-04 19:51:14 +0500

    изменю строку 42

---
+++ src/personalfinance/settings/Settings.java
@@ -0,0 +1,71 @@
+package personalfinance.settings;
+
+import org.ini4j.Ini;
+import org.ini4j.IniPreferences;
+import org.ini4j.Wini;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.prefs.Preferences;
+
+public class Settings {
+
+    public static final File FONT_ROBOTO_LIGHT = new File("fonts/Roboto-Light.ttf");
+    public static final File SAVE_DIR = new File("saves/");
+    public static final String SAVE_FILE_EXT = "kmkpf";
+
+    public static final String FORMAT_AMOUNT ="%.2f"; //    константа, отвечающая за формат сохранения данных
+    public static final String FORMAT_RATE = "%.5f";
+    public static final String FORMAT_DATE = "dd.MM.yyyy";
+    public static final String FORMAT_DATE_MONTH = "MMMM yyyy";
+    public static final String FORMAT_DATE_ = "yyyy";
+
+    public static final String[] CURRENCIES_CODES = new String[]{"RUB", "USD", "EUR", "BYN", "UAH"};
+
+    public static final int COUNT_OVERVIEW_ROWS = 10; // константа отвечает за отображение количества строк
+
+    private static final File FILE_SETTINGS = new File("saves/settings.ini"); // сохранение настроек, путь к последнему сохраненному файлу
+
+    private static File FILE_SAVE = new File("saves/default.kmkpf");
+
+    public static void init() {
+        try {
+            Ini ini = new Ini(FILE_SETTINGS);
+            Preferences prefs =new IniPreferences(ini);
+            String file = prefs.node("Settings").get("FILE_SAVE", null);
+            if (file != null) FILE_SAVE = new File(file);
+            setLocale();
+        } catch (IOException e) {
+            Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+    }
+
+    public static File getFileSave() {
+        return FILE_SAVE;
+    }
+
+    public static void setFileSave(File fileSave) {
+        FILE_SAVE = fileSave;
+        save();
+    }
+
+    private static void save() {
+        try {
+            Wini ini = new Wini(FILE_SETTINGS);
+            ini.put("Settings", "FILE_SAVE", FILE_SAVE.getAbsolutePath().replace("\\", "\\\\"));
+            ini.store();
+        } catch (IOException e) {
+            Logger.getLogger(Settings.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+
+    private static void setLocale() {
+        Locale.setDefault(new Locale("ru"));
+    }
+
+}
