d22e40c266279869f8534d2d09f7aecf5b793a49
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-05-10 02:50:49 +0500

    Создан абстрактный класс для ToolBar

---
+++ src/personalfinance/gui/toolbar/AbstractToolBar.java
@@ -0,0 +1,35 @@
+package personalfinance.gui.toolbar;
+
+import personalfinance.gui.MainButton;
+import personalfinance.gui.Refresh;
+
+import javax.swing.*;
+
+abstract public class AbstractToolBar extends JPanel implements Refresh {
+
+    public AbstractToolBar() {
+
+    }
+
+    abstract protected void init();
+
+    protected MainButton addButton(String title, ImageIcon icon, String action, boolean topIcon) {
+        MainButton button = new MainButton(title, icon, null, action);
+        if (topIcon) {
+            button.setHorizontalTextPosition(SwingConstants.CENTER);
+            button.setVerticalTextPosition(SwingConstants.BOTTOM);
+        } else {
+            button.setHorizontalTextPosition(SwingConstants.RIGHT);
+            button.setVerticalTextPosition(SwingConstants.CENTER);
+        }
+        add(button);
+        return button;
+    }
+
+    @Override
+    public void refresh() {
+        removeAll();
+        init();
+    }
+
+}
