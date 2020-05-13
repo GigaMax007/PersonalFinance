bc08e6d0c0e13f907a43b0437618d9a290588855
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-05-14 00:02:19 +0500

    Дописан абстрактный клас для создания и редактирования диалоговых окон

--- src/personalfinance/gui/dialog/AddEditDialog.java
+++ src/personalfinance/gui/dialog/AddEditDialog.java
@@ -1,12 +1,18 @@
 package personalfinance.gui.dialog;
 
+import org.jdatepicker.impl.JDatePickerImpl;
+import org.jdatepicker.impl.UtilDateModel;
 import personalfinance.exception.ModelException;
+import personalfinance.gui.MainButton;
 import personalfinance.gui.MainFrame;
 import personalfinance.model.Common;
+import personalfinance.settings.HandlerCode;
 import personalfinance.settings.Style;
 import personalfinance.settings.Text;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -41,7 +47,7 @@ public abstract class AddEditDialog extends JDialog {
         components.clear();
         icons.clear();
         values.clear();
-        dispose();
+        dispose(); // очистим оперативную память
     }
 
     public boolean isAdd() {
@@ -56,13 +62,13 @@ public abstract class AddEditDialog extends JDialog {
 
     private void setDialog() {
         init();
-        if(isAdd()) setTitle(Text.get("ADD"));
+        if (isAdd()) setTitle(Text.get("ADD"));
         else {
             setVelues();
             setTitle(Text.get("EDIT"));
         }
         getContentPane().removeAll(); // Очищение панели
-        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)); // Элементы с вертикальным выравниванием по оси Y
         ((JPanel) getContentPane()).setBorder(Style.BORDER_DIALOG);
 
         for (Map.Entry<String, JComponent> entry :
@@ -75,7 +81,52 @@ public abstract class AddEditDialog extends JDialog {
             JComponent jComponent = entry.getValue();
             if (jComponent instanceof JTextField) {
                 jComponent.setPreferredSize(Style.DIMENSION_DIALOG_TEXTFIELD_SIZE);
+                if (values.containsKey(key)) ((JTextField) jComponent).setText("" + values.get(key));
+            } else if (jComponent instanceof JComboBox) {
+                if (values.containsKey(key)) ((JComboBox) jComponent).setSelectedItem("" + values.get(key));
+            } else if (jComponent instanceof JDatePickerImpl) {
+                if (values.containsKey(key))
+                    ((UtilDateModel) ((JDatePickerImpl) jComponent).getModel()).setValue((Date) values.get(key));
             }
+            jComponent.setAlignmentX(JComponent.LEFT_ALIGNMENT);
+            add(label);
+            add(Box.createVerticalStrut(Style.PADDING_DIALOG));
+            add(jComponent);
+            add(Box.createVerticalStrut(Style.PADDING_DIALOG));
+
+        }
+        MainButton ok = new MainButton(Text.get("ADD"), Style.ICON_OK, null, HandlerCode.ADD);
+        if (!isAdd()) {
+            ok.setActionCommand(HandlerCode.EDIT);
+            ok.setText(Text.get("EDIT"));
+        }
+
+        MainButton cancel = new MainButton(Text.get("CANCEL"), Style.ICON_CANCEL, null, HandlerCode.CANCEL);
+
+        JPanel panelButton = new JPanel();
+        panelButton.setLayout(new BorderLayout());
+        panelButton.add(ok, BorderLayout.WEST);
+
+        panelButton.add(cancel, BorderLayout.EAST);
+
+        add(panelButton);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    protected class CommonComboBox extends JComboBox {
+
+        public CommonComboBox(Object[] objs) {
+            super(objs);
+            setRenderer(new DefaultListCellRenderer() {
+                @Override
+                public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+                    DefaultListCellRenderer renderer = (DefaultListCellRenderer) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+                    Common c = (Common) value;
+                    if (c != null) renderer.setText(c.getValueForComboBox());
+                    return renderer;
+                }
+            });
         }
     }
 }
