029838072250ac2faf3ad1461a4407888ac7e213
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-05-15 21:15:02 +0500

    Создан классы  для переводов на правой панели

---
+++ src/personalfinance/gui/panel/TransferPanel.java
@@ -0,0 +1,13 @@
+package personalfinance.gui.panel;
+
+import personalfinance.gui.MainFrame;
+import personalfinance.gui.table.TranferTableData;
+import personalfinance.gui.toolbar.FunctionsToolBar;
+import personalfinance.settings.Style;
+
+public class TransferPanel extends RightPanel {
+
+    public TransferPanel(MainFrame frame) {
+        super(frame, new TranferTableData(), "TRANSFERS", Style.ICON_PANEL_TRANSFERS, new FunctionsToolBar());
+    }
+}
