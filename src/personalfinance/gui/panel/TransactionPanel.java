73a594df7b03d10d2a7318e8ef9e9cd130295b98
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-05-16 00:19:32 +0500

    Добавлен фильтр на панели переводов и транзакций

--- src/personalfinance/gui/panel/TransactionPanel.java
+++ src/personalfinance/gui/panel/TransactionPanel.java
@@ -5,9 +5,12 @@ import personalfinance.gui.table.TransactionTableData;
 import personalfinance.gui.toolbar.FunctionsToolBar;
 import personalfinance.settings.Style;
 
+import javax.swing.*;
+
 public class TransactionPanel extends RightPanel {
 
     public TransactionPanel(MainFrame frame) {
-        super(frame, new TransactionTableData(), "TRANSACTIONS", Style.ICON_PANEL_TRANSACTIONS, new FunctionsToolBar());
+        super(frame, new TransactionTableData(), "TRANSACTIONS", Style.ICON_PANEL_TRANSACTIONS,
+                new JPanel[] {new FunctionsToolBar(), new FilterPanel(frame)});
     }
 }
--- src/personalfinance/gui/panel/TransferPanel.java
+++ src/personalfinance/gui/panel/TransferPanel.java
@@ -5,9 +5,12 @@ import personalfinance.gui.table.TranferTableData;
 import personalfinance.gui.toolbar.FunctionsToolBar;
 import personalfinance.settings.Style;
 
+import javax.swing.*;
+
 public class TransferPanel extends RightPanel {
 
     public TransferPanel(MainFrame frame) {
-        super(frame, new TranferTableData(), "TRANSFERS", Style.ICON_PANEL_TRANSFERS, new FunctionsToolBar());
+        super(frame, new TranferTableData(), "TRANSFERS", Style.ICON_PANEL_TRANSFERS,
+                new JPanel[] {new FunctionsToolBar(), new FilterPanel(frame)});
     }
 }
