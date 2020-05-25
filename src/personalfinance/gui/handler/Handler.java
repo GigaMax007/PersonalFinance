92259f6ba37bc8d0ffdc7cc012ef50351f441d73
Maksim Kishinskiy <m.kishinskiy@gmail.com>
2020-05-25 23:54:51 +0500

    Обработали меню Файл

---
+++ src/personalfinance/gui/handler/Handler.java
@@ -0,0 +1,20 @@
+package personalfinance.gui.handler;
+
+import personalfinance.gui.MainFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public abstract class Handler implements ActionListener {
+
+    protected final MainFrame frame;
+
+    public Handler(MainFrame frame) {
+        this.frame = frame;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        frame.refresh();
+    }
+}
